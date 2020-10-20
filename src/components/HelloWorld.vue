2bfaa9fde80820bc9fb53ad49a9c22e80757fbb8
Taras <ter4444@gmail.com>
2020-10-20 14:19:09 +0300

    updated @wellcode/core to 0.0.3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,46 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <section>
+    <p class="content"><b>Selected:</b> {{ selectedItem }}</p>
+    <b-field label="Find or add the town">
+      <b-autocomplete
+        rounded
+        clearable
+        :loading="isFetching"
+        v-model="userInput"
+        ref="autocomplete"
+        :data="filteredSuggestions"
+        placeholder="e.g. Geneve"
+        @select="option => (selectedItem = option)"
+        @input="onChange"
+      >
+        <template slot="header">
+          <a @click="addTown">
+            <span> Add current town </span>
+          </a> 
+        </template>
+        <template v-if="error" slot="empty">Error occured on server</template>
+        <template v-else slot="empty">No results for {{userInput}}</template>
+      </b-autocomplete>
+    </b-field>
+  </section>
 </template>
 
 <script>
+import { useTownAutocomplete } from '@wellcode/core';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  setup() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = `${proxy}https://webservices.post.ch:17023/IN_SYNSYN_EXT/REST/v1/autocomplete2`
+    return {
+      ...useTownAutocomplete(apiUrl)
+    }
+  },
+  created() {
+  },
+  methods: {
+    addTown() {
+      this.$refs.autocomplete.setSelected(this.userInput)
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueCompositionAPI from '@vue/composition-api'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
+Vue.use(Buefy)
+Vue.use(VueCompositionAPI)
 Vue.config.productionTip = false
 
 new Vue({
